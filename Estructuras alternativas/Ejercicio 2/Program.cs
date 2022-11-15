46c6670c8981377e866290f88412d91c5315ab58
Edwin Viña R <111668890+ecodant@users.noreply.github.com>
2022-11-15 13:03:31 +0000

    Ading ejercicio 1

---
+++ Estructuras alternativas/Ejercicio 2/Program.cs
@@ -0,0 +1,27 @@
+﻿int nota, age;
+string sex;
+
+Console.WriteLine("Ingrese la nota como entero porfavor");
+nota = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la edad como entero porfavor");
+age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese su sexo, 'M' para masculino y 'F' para femenino");
+sex = (Console.ReadLine());
+
+if (nota >= 5 && age >= 18)
+{
+    if (sex == "F" || sex == "f")
+    {
+        Console.WriteLine("ACEPTADA");
+    }
+    else if (sex == "M" || sex == "m")
+    {
+        Console.WriteLine("POSIBLE");
+    }
+}
+else
+{
+    Console.WriteLine("NO ACEPTADA");
+}
