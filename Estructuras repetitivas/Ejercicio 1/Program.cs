55dce09f42cfa2f4274be7757fdf38125f35aff8
Edwin Viña R <111668890+ecodant@users.noreply.github.com>
2022-11-15 14:31:20 +0000

    Adding repetitivas, ejercicio 1 y 2

---
+++ Estructuras repetitivas/Ejercicio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿int n;
+long factorial = 1;
+bool validate = false;
+
+while (!validate)
+{
+    Console.WriteLine("Ingrese un valor entero positivo para averiguar su respectivo factorial");
+
+    if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Operación completada → {n}! = {factorial}");
+        Console.ResetColor();
+        validate = true;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("**ERROR, POR FAVOR, INGRESE UN VALOR VALIDO");
+        Console.ResetColor();
+    }
+}
---
+++ Estructuras repetitivas/Ejercicio 2/Program.cs
@@ -0,0 +1,44 @@
+﻿int n, intentos = 10;
+Random randomNumber = new Random();
+int guess = randomNumber.Next(1,101);
+
+// Console.WriteLine(guess);
+while (intentos > 0)
+{ 
+    Console.WriteLine("Ingrese un número del 1 al 100");
+    if (int.TryParse(Console.ReadLine(), out n))
+    {
+        if(n == guess)
+        {
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine($"Felicidades ha acertado!!, con {11 - intentos} intentos");
+            Console.ResetColor();
+            intentos = 0;
+        }
+        else if (n > guess)
+        {
+            Console.WriteLine($"Usted ingreso {n}, el número a adivinar era menor que el ingresado");
+            intentos--;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Le quedan {intentos} intentos");
+            Console.ResetColor(); 
+        }
+        else
+        {
+            Console.WriteLine($"Usted ingreso {n}, el número a adivinar era mayor que el ingresado");
+            intentos--;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Le quedan {intentos} intentos");
+            Console.ResetColor();
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error, por favor, ingrese un número entero");
+        Console.ResetColor();
+    }
+}
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"El número era {guess}");
+Console.ResetColor();
