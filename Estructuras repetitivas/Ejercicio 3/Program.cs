87381fec182bd9e7865adf0e55a72722b9016ea4
Edwin Viña R <111668890+ecodant@users.noreply.github.com>
2022-11-15 15:01:36 +0000

    Adding ejercicio 3 y 4

---
+++ Estructuras repetitivas/Ejercicio 3/Program.cs
@@ -0,0 +1,32 @@
+﻿int nMayor = 0, nMenor = 0;
+
+Console.WriteLine("Ingrese el primer número");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número");
+int n2 = int.Parse(Console.ReadLine());
+if (n1 == n2)
+{
+    Console.WriteLine("Los números son iguales");
+}
+else if (n1 > n2)
+{
+    nMayor= n1;
+    nMenor= n2;
+}
+else
+{
+    nMayor= n2;
+    nMenor= n1;
+}
+Console.WriteLine("Los números pares entre los números que proporcionó son: ");
+Console.ForegroundColor = ConsoleColor.Green;
+for (int i = nMenor; i <= nMayor; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+}
+Console.ResetColor();
+Console.Write("\n");
---
+++ Estructuras repetitivas/Ejercicio 4/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = 1, sum = 0, prom = 0, nValues = 0;
+while (n != 0)
+{
+    Console.WriteLine("Ingrese un número");
+    if (int.TryParse(Console.ReadLine(), out n))
+    {
+        if (n > 0)
+        {
+            sum += n;
+            nValues++;
+            prom = sum / nValues;
+        }
+    }
+    else
+    {
+        n = 1;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error, no ingreso un número");
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine($"La suma de los valores ingresados es: {sum} y el promedio es: {prom}");
