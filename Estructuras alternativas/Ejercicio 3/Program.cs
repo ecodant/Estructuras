152cbe581c2ca57da6062f6efe62e06ef5b8d7a4
Edwin Viña R <111668890+ecodant@users.noreply.github.com>
2022-11-15 14:19:16 +0000

    Adding Ejercicio 3 y 4

---
+++ Estructuras alternativas/Ejercicio 3/Program.cs
@@ -0,0 +1,20 @@
+﻿    int[] numbers= new int[3];
+    Console.WriteLine("Ingrese primer numero");
+    numbers[0] = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese segundo numero");
+    numbers[1] = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese tercer numero");
+    numbers[2] = int.Parse(Console.ReadLine());
+    
+    Array.Sort(numbers);
+    Array.Reverse(numbers);
+
+        for (int i = 0; i < 3;i++)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(numbers[i] + " ");
+            Console.ResetColor();
+        }
+        Console.Write("\n");
---
+++ Estructuras alternativas/Ejercicio 4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a,b,c;
+
+Console.WriteLine("Ingrese el valor A del traingulo");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor B del traingulo");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor C del traingulo");
+c = int.Parse(Console.ReadLine());
+if (((a*a) + (b*b)) == (c*c))
+{
+    Console.WriteLine("Es un triangulo rectangulo"); 
+}
+
+else if((a == b && a != c) || (a == c && a != b))
+{
+    Console.WriteLine("Es un triangulo isósceles"); 
+}
+
+else if (a == b && b == c)
+{
+    Console.WriteLine("Es un triangulo Equilatero"); 
+}
+
+else 
+{
+    Console.WriteLine("Es un triangulo escaleno"); 
+}
